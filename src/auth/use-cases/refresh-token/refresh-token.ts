f76c85b6bf85061a7219ae90ef6b3ce67f351f3f
shimarrudz <vic.shima.vss@gmail.com>
2023-07-16 15:44:46 -0300

    feat: refresh class

--- src/auth/use-cases/refresh-token/refresh-token.ts
+++ src/auth/use-cases/refresh-token/refresh-token.ts
@@ -3,3 +3,10 @@ import { JwtService } from "@nestjs/jwt";
 import { RefreshTokenRepository } from "src/auth/infra/repositories/prisma";
 import { IUserToken, IJwtPayload, IUserFromJwt } from "src/auth/interfaces";
 
+@Injectable()
+export class Refresh {
+    constructor (
+        private jwtService: JwtService,
+        private refreshTokenRepository: RefreshTokenRepository,
+    ) {}
+}
