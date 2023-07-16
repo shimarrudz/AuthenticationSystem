e76fa143e9458c6881710777514b5cc2e970455a
shimarrudz <vic.shima.vss@gmail.com>
2023-07-16 16:12:30 -0300

    feat: refresh tkens methods

--- src/auth/use-cases/refresh-token/refresh-token.ts
+++ src/auth/use-cases/refresh-token/refresh-token.ts
@@ -1,26 +1,57 @@
-import { Injectable } from "@nestjs/common";
-import { JwtService } from "@nestjs/jwt";
-import { RefreshTokenRepository } from "src/auth/infra/repositories/prisma";
-import { IUserToken, IJwtPayload, IUserFromJwt } from "src/auth/interfaces";
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { RefreshTokenRepository } from 'src/auth/infra/repositories/prisma';
+import { IUserToken, IJwtPayload, IUserFromJwt } from '../../interfaces';
 
 @Injectable()
 export class Refresh {
-    constructor (
-        private jwtService: JwtService,
-        private refreshTokenRepository: RefreshTokenRepository,
-    ) {}
-}
+  constructor(
+    private jwtService: JwtService,
+    private refreshTokenRepository: RefreshTokenRepository,
+  ) {}
 
-    async execute(refreshToken: string): Promise<IUserToken> {
-        const user = await this.validateRefreshToken(refreshToken);
+  async execute(refreshToken: string): Promise<IUserToken> {
+    const user = await this.validateRefreshToken(refreshToken);
 
-        const accessToken = this.generateAccessToken(user);
-        const newRefreshToken = await this.generateRefreshToken(user);
-    
-        return {
-            accessToken,
-            refreshToken: newRefreshToken,
-        };
+    const accessToken = this.generateAccessToken(user);
+    const newRefreshToken = await this.generateRefreshToken(user);
+
+    return {
+      accessToken,
+      refreshToken: newRefreshToken,
+    };
+  }
+
+  private async validateRefreshToken(refreshToken: string): Promise<IUserFromJwt> {
+    const payload = this.jwtService.verify<IJwtPayload>(refreshToken);
+
+    const user = await this.refreshTokenRepository.findUserByRefreshToken(payload.sub, refreshToken);
+
+    if (!user) {
+      throw new UnauthorizedException('Refresh token inválido!');
     }
 
-    
+    return user;
+  }
+
+  private generateAccessToken(user: IUserFromJwt): string {
+    const payload: IJwtPayload = {
+      sub: user.id,
+      email: user.email,
+    };
+
+    return this.jwtService.sign(payload, {
+      expiresIn: '5m',
+    });
+  }
+
+  private async generateRefreshToken(user: IUserFromJwt): Promise<string> {
+    const refreshTokenExpiresIn = 2 * 24 * 60 * 60 * 500;
+    const refreshToken = await this.refreshTokenRepository.createRefreshToken({
+      user_id: user.id,
+      expires_at: refreshTokenExpiresIn,
+    });
+
+    return refreshToken.token;
+  }
+}
