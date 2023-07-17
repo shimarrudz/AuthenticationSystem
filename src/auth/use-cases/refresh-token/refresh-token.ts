111cd89cc0e0c05ee11eb9b57217dc9ebcc71726
shimarrudz <vic.shima.vss@gmail.com>
2023-07-17 09:25:42 -0300

    feat: import

--- src/auth/use-cases/refresh-token/refresh-token.ts
+++ src/auth/use-cases/refresh-token/refresh-token.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { RefreshTokenRepository } from 'src/auth/infra/repositories/prisma';
 import { IUserToken, IJwtPayload, IUserFromJwt } from '../../interfaces';
