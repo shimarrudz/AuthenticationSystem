97258ac78c0e3df32a72eaec585e4ba0bdc44adc
shimarrudz <vic.shima.vss@gmail.com>
2023-07-20 15:28:47 -0300

    feat: created asyncs

--- src/token/domain/interfaces/refresh-token-repository.ts
+++ src/token/domain/interfaces/refresh-token-repository.ts
@@ -3,6 +3,5 @@ import { RefreshToken } from "@prisma/client";
 
 export abstract class IRefreshTokenRepository {
   abstract findRefreshToken(token: string): Promise<RefreshToken>;
-  abstract revokeRefreshToken(token: string): Promise<void>;
   abstract findUserById(userId: string): Promise<UserFromJwtDto | null>
 }
--- src/token/domain/test/in-memory/in-memory-user-repository.ts
+++ src/token/domain/test/in-memory/in-memory-user-repository.ts
@@ -1,6 +1,7 @@
 import { UserFromJwtDto } from "@/auth/domain/dto";
 import { IRefreshTokenRepository } from "../../interfaces";
 import { PrismaClient, RefreshToken } from "@prisma/client";
+import { GetResult } from "@prisma/client/runtime/library";
 
 export class InMemoryRefreshToken implements IRefreshTokenRepository {
     private prisma: PrismaClient;
@@ -9,6 +10,28 @@ export class InMemoryRefreshToken implements IRefreshTokenRepository {
     constructor() {
         this.prisma = new PrismaClient();
         this.refreshTokens = [];
+    }
+    
+    async findUserById(userId: string): Promise<UserFromJwtDto | null> {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: userId,
+            },
+            select: {
+                id: true,
+                email: true,
+            },
+        });
+
+        return user ? { id: user.id, email: user.email } : null;
+    }
+
+    async findRefreshToken(token: string): Promise<RefreshToken | null> {
+        const refreshToken = this.refreshTokens.find(
+            (refreshToken) => refreshToken.token === token 
+        );
+        
+        return refreshToken || null;
     }
     
 }
