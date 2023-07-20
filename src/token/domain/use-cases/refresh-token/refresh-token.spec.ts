d967bf422d2c4225342a879613fd474dfbe4642e
shimarrudz <vic.shima.vss@gmail.com>
2023-07-20 15:55:32 -0300

    feat: refresh token tests

--- src/token/domain/use-cases/refresh-token/refresh-token.spec.ts
+++ src/token/domain/use-cases/refresh-token/refresh-token.spec.ts
@@ -1,6 +1,6 @@
 import { JwtService } from "@nestjs/jwt";
 import { UnauthorizedException, NotFoundException } from "@nestjs/common";
-import * as jwt from 'jsonwebtoken'
+import * as jwt from "jsonwebtoken";
 
 import { Refresh } from "./refresh-token";
 import { IRefreshTokenRepository } from "../../interfaces";
@@ -8,16 +8,47 @@ import { RefreshPayloadTokenDto } from "../../interfaces";
 import { InMemoryRefreshToken } from "../../test/in-memory/in-memory-user-repository";
 
 describe("Refresh", () => {
-    let refreshUseCase: Refresh;
-    let jwtService: JwtService;
-    let inMemoryUserRepository: InMemoryRefreshToken;
+  let refreshUseCase: Refresh;
+  let jwtService: JwtService;
+  let refreshTokenRepository: IRefreshTokenRepository;
 
-    beforeEach(() => {
-        inMemoryUserRepository = new InMemoryRefreshToken();
+  beforeEach(() => {
+    refreshTokenRepository = new InMemoryRefreshToken();
+    refreshUseCase = new (jwtService, refreshTokenRepository);
 
-        jwtService = {
-            sign: jest.fn(),
-        } as any;
+    jwtService = {
+      sign: jest.fn(),
+    } as any;
+  });
 
-    })
-})
+  describe("Success Test", () => {
+    it("should refresh token and return a new access token", async () => {
+        const refreshToken = "QWNDsakj314j32oirjsdadsa2ij";
+
+        const fakeUser = {
+            id: "123456",
+            email: "JohnDoe@gmail.com",
+        };
+
+        jest.spyOn(refreshTokenRepository, "findRefreshToken").mockResolvedValue({
+            token: refreshToken,
+        } as any);
+
+        const newAccessToken = "QWNDsakj314j3dsaoijd90dsa";
+        (jwtService.sign as jest.Mock).mockReturnValue(newAccessToken);
+
+        const result: RefreshPayloadTokenDto = await refreshUseCase.execute(refreshToken);
+
+        expect(result).toEqual({
+            accessToken: newAccessToken,
+        });
+
+        expect(refreshTokenRepository.findRefreshToken).toBeCalledWith(refreshToken);
+
+        expect(jwtService.sign).toHaveBeenCalledWith(
+            { sub: fakeUser.id, email: fakeUser.email },
+            { expiresIn: '5m' },
+        );
+    });
+  });
+});
